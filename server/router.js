95505d4427581fa903361c66d6fe0929fbae39a6
Izzedeen Alfarra <z4dev@Izzedeens-MacBook-Pro.local>
2024-07-25 12:51:57 +0300

    feat: adding email module

--- server/router.js
+++ server/router.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 import userRouter from './users/routers/index.js';
+import emailRouter from './email/routes/index.js';
 
 const router = express.Router();
 
 
 router.use('/users', userRouter);
-
+router.use('/email', emailRouter);
 
 router.use('/health', (req, res) => {
   const data = {
