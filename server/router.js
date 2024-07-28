f1d12158b8f16e273965f05687cec3e3e912d6b2
Martini <martinashraf99@gmail.com>
2024-07-28 18:45:23 +0300

    refactor: renaming the url

--- server/router.js
+++ server/router.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.use('/users', userRouter);
 
 
-router.use('/notification', notificationRouter);
+router.use('/notifications', notificationRouter);
 
 
 router.use('/health', (req, res) => {
