724eab7198ee7ce3ecc0af9baad76db970df6414
Izzedeen Alfarra <z4dev@Izzedeens-MacBook-Pro.local>
2024-10-01 10:26:02 +0300

    feat : version-1 products

---
+++ server/products/models/index.js
@@ -0,0 +1,64 @@
+import ProductSchema from '../schema/index.js';
+
+class Product {
+  async find(selectors = {}, options = {}, projection = {}) {
+    const result = await ProductSchema.find(selectors, projection, options)
+      .lean()
+      .maxTimeMS(60000);
+    return result;
+  }
+
+  async findOne(selector = {}, projection = {}) {
+    const result = await ProductSchema.findOne(selector).select(projection).lean();
+    return result;
+  }
+
+  async count(selectors = {}) {
+    const result = await ProductSchema.countDocuments(selectors).maxTimeMS(60000);
+    return result;
+  }
+
+  async create(payload) {
+    const result = await ProductSchema.create(payload);
+    return result;
+  }
+
+  async update(selector = {}, newParams, options = {}) {
+    const result = await ProductSchema.findOneAndUpdate(selector, newParams, {
+      runValidators: true,
+      new: true,
+      ...options
+    });
+    return result;
+  }
+
+  async updateMany(selector, newParams, options = {}) {
+    const result = await ProductSchema.updateMany(selector, newParams, options);
+    return result;
+  }
+  async delete(selector, options = {}) {
+    const result = await ProductSchema.deleteOne(selector, options);
+    return result;
+  }
+
+  async deleteMany(selector, options = {}) {
+    const result = await ProductSchema.deleteMany(selector, options);
+    return result;
+  }
+
+  async aggregate(pipeline, options = {}) {
+    const result = await ProductSchema.aggregate(pipeline, { maxTimeMS: 60000 })
+      .sort(options.sort || 'createdAt')
+      .skip(options.skip || 0)
+      .limit(options.limit || 200);
+    return result;
+  }
+  async countDocuments(selector = {}) {
+    const result = await ProductSchema.countDocuments(selector);
+    return result;
+  }
+}
+
+
+
+export default new Product();
---
+++ server/products/validations/index.js
@@ -0,0 +1,74 @@
+import Joi from 'joi';
+import { CONTROLLERS } from '../helpers/constants.js';
+export default {
+  [CONTROLLERS.LIST_PRODUCTS]: {
+    query: Joi.object()
+      .keys({
+        page: Joi.number().optional(),
+        limit: Joi.number().optional(),
+        minPrice: Joi.number().optional(),
+        maxPrice: Joi.number().optional()
+      })
+      .optional()
+  },
+  [CONTROLLERS.GET_PRODUCT]: {
+    params: Joi.object()
+      .keys({
+        id: Joi.string().required()
+      })
+      .required()
+  },
+  [CONTROLLERS.CREATE_PRODUCT]: {
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      description: Joi.string().allow(''),
+      packageId: Joi.string().optional().default('undefined'),
+      addedBy: Joi.forbidden(),
+      availableQuantity: Joi.number().required(),
+      price: Joi.object().keys({
+        originalPrice: Joi.number().required(),              
+        finalPrice: Joi.number()
+          .required()
+          .less(Joi.ref('originalPrice')) 
+          .messages({
+            'any.less': 'Final price must be less than the original price.'
+          })
+      }),
+      images: Joi.array().items(Joi.string()),
+      tags: Joi.array().items(Joi.string())
+    })
+  },
+  [CONTROLLERS.UPDATE_PRODUCT]: {
+    params: Joi.object()
+      .keys({
+        id: Joi.string().required() 
+      })
+      .required(),
+    
+    body: Joi.object().keys({
+      name: Joi.string(),                          
+      description: Joi.string().allow(''),         
+      packageId: Joi.string().default('undefined'),
+      availableQuantity: Joi.number(),            
+      price: Joi.object().keys({
+        originalPrice: Joi.number().required(),              
+        finalPrice: Joi.number()
+          .required()
+          .less(Joi.ref('originalPrice')) 
+          .messages({
+            'any.less': 'Final price must be less than the original price.'
+          })
+      }),
+      images: Joi.array().items(Joi.string()),     
+      tags: Joi.array().items(Joi.string())        
+    }).or('name', 'description', 'packageId', 'availableQuantity', 'price', 'images', 'tags') // Ensures at least one field is provided
+  },
+  
+  [CONTROLLERS.DELETE_PRODUCT]: {
+    params: Joi.object()
+      .keys({
+        id: Joi.string().required()
+      })
+      .required()
+  },
+};
