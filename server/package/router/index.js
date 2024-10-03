c4cee51f1010fe55a79d92654b68a4dbc118f124
Izzedeen Alfarra <z4dev@Izzedeens-MacBook-Pro.local>
2024-10-03 10:20:42 +0300

    ready for hosting

---
+++ server/package/router/index.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import { CONTROLLERS } from '../helpers/constants.js';
+import Controller from '../controllers/index.js';
+import validationSchemas from '../validations/index.js';
+import validateRequest from '../../../common/middleware/requestValidation/index.js';
+import Authenticate from '../../../common/middleware/authentication/index.js';
+import Authorization from '../../../common/middleware/authorization/index.js';
+import Permissions from '../permissions.js';
+
+const router = express.Router();
+
+router.get(
+  '/',
+  Authenticate,
+  Authorization.Authorize(Permissions[CONTROLLERS.LIST_PACKAGES]),
+  validateRequest(validationSchemas[CONTROLLERS.LIST_PACKAGES]),
+  Controller[CONTROLLERS.LIST_PACKAGES]
+);
+
+router.get(
+  '/:id',
+  Authenticate,
+  Authorization.Authorize(Permissions[CONTROLLERS.GET_PACKAGE]),
+  validateRequest(validationSchemas[CONTROLLERS.GET_PACKAGE]),
+  Controller[CONTROLLERS.GET_PACKAGE]
+);
+
+router.post(
+  '/',
+  Authenticate,
+  Authorization.Authorize(Permissions[CONTROLLERS.CREATE_PACKAGE]),
+  validateRequest(validationSchemas[CONTROLLERS.CREATE_PACKAGE]),
+  Controller[CONTROLLERS.CREATE_PACKAGE]
+);
+
+router.put(
+  '/:id',
+  Authenticate,
+  Authorization.Authorize(Permissions[CONTROLLERS.UPDATE_PACKAGE]),
+  validateRequest(validationSchemas[CONTROLLERS.UPDATE_PACKAGE]),
+  Controller[CONTROLLERS.UPDATE_PACKAGE]
+);
+
+router.delete(
+  '/:id',
+  Authenticate,
+  Authorization.Authorize(Permissions[CONTROLLERS.DELETE_PACKAGE]),
+  validateRequest(validationSchemas[CONTROLLERS.DELETE_PACKAGE]),
+  Controller[CONTROLLERS.DELETE_PACKAGE]
+);
+
+export default router;
---
+++ server/package/validations/index.js
@@ -0,0 +1,60 @@
+import Joi from 'joi';
+import { CONTROLLERS } from '../helpers/constants.js';
+
+export default {
+  [CONTROLLERS.LIST_PACKAGES]: {
+    query: Joi.object({
+      page: Joi.number().optional(),
+      limit: Joi.number().optional()
+    })
+  },
+  [CONTROLLERS.GET_PACKAGE]: {
+    params: Joi.object({
+      id: Joi.string().required()
+    })
+  },
+  [CONTROLLERS.CREATE_PACKAGE]: {
+    body: Joi.object({
+      name: Joi.string().required(),
+      description: Joi.string().optional(),
+      price: Joi.object({
+        originalPrice: Joi.number().required(),
+        finalPrice: Joi.number().required().when('originalPrice', {
+          is: Joi.number(),
+          then: Joi.number().max(Joi.ref('originalPrice')).messages({
+            'number.max': 'finalPrice must be less than or equal to originalPrice',
+          }),
+          otherwise: Joi.number().required(),
+        }),
+      }).required(),
+    
+      availableQuantity: Joi.number().required()
+    })
+  },
+  [CONTROLLERS.UPDATE_PACKAGE]: {
+    params: Joi.object({
+      id: Joi.string().required()
+    }),
+    body: Joi.object({
+      name: Joi.string().optional(),
+      description: Joi.string().optional(),
+      price: Joi.object({
+        originalPrice: Joi.number().required(),
+        finalPrice: Joi.number().required().when('originalPrice', {
+          is: Joi.number(),
+          then: Joi.number().max(Joi.ref('originalPrice')).messages({
+            'number.max': 'finalPrice must be less than or equal to originalPrice',
+          }),
+          otherwise: Joi.number().required(),
+        }),
+      }).optional(),
+      availableQuantity: Joi.number().optional()
+    })
+  },
+
+  [CONTROLLERS.DELETE_PACKAGE]: {
+    params: Joi.object({
+      id: Joi.string().required()
+    })
+  }
+};
