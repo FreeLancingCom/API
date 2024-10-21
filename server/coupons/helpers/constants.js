435941c23551571e984d9d1c0f07f7b21119c358
Izzedeen Alfarra <z4dev@Izzedeens-MacBook-Pro.local>
2024-10-21 13:53:08 +0300

    refactor:solve the issue

---
+++ server/coupons/helpers/constants.js
@@ -0,0 +1,53 @@
+export const CONTROLLERS = {
+  LIST_COUPONS: 'LIST_COUPONS',
+  GET_COUPON: 'GET_COUPON',
+  CREATE_COUPON: 'CREATE_COUPON',
+  UPDATE_COUPON: 'UPDATE_COUPON',
+  DELETE_COUPON: 'DELETE_COUPON',
+  APPLY_COUPON: 'APPLY_COUPON'
+};
+
+export const COUPON_TYPES = {
+  PERCENTAGE: 'PERCENTAGE',
+  FIXED: 'FIXED'
+};
+
+export const COUPON_STATUS = {
+  ACTIVE: 'ACTIVE',
+  INACTIVE: 'INACTIVE'
+};
+
+export const couponsErrors = {
+  COUPON_NOT_FOUND: {
+    message: 'Coupon not found',
+    code: 101
+  },
+  COUPON_ALREADY_EXISTS: {
+    message: 'Coupon already exists',
+    code: 102
+  },
+  INVALID_COUPON_TYPE: {
+    message: 'Invalid coupon type',
+    code: 103
+  },
+  INVALID_COUPON_STATUS: {
+    message: 'Invalid coupon status',
+    code: 104
+  },
+  INVALID_COUPON_CODE: {
+    message: 'Invalid coupon code',
+    code: 105
+  },
+  INVALID_COUPON_DISCOUNT: {
+    message: 'Invalid coupon discount',
+    code: 106
+  },
+  INVALID_COUPON_EXPIRY_DATE: {
+    message: 'Invalid coupon expiry date',
+    code: 107
+  },
+  COUPON_ALREADY_EXIST_AND_NOT_EXPIRED: {
+    message: 'Coupon already exists and has not expired',
+    code: 108
+  }
+};
---
+++ server/coupons/services/couponService.js
@@ -0,0 +1,184 @@
+import Coupon from '../models/index.js';
+import { getPaginationAndSortingOptions } from '../../../common/utils/pagination/index.js';
+import ErrorResponse from '../../../common/utils/errorResponse/index.js';
+
+import moment from 'moment';
+
+import { COUPON_STATUS, couponsErrors , COUPON_TYPES } from '../helpers/constants.js';
+
+import StatusCodes from 'http-status-codes';
+const { BAD_REQUEST } = StatusCodes;
+
+import logger from '../../../common/utils/logger/index.js';
+
+import _ from 'lodash';
+
+class CouponService {
+  async listCoupons(query) {
+    const { limit, skip, sort, ..._query } = query;
+    const options = getPaginationAndSortingOptions(query);
+    try {
+      const coupons = await Coupon.find(_query, options);
+      return { coupons, options };
+    } catch (e) {
+      logger.error(e);
+      throw e;
+    }
+  }
+
+  async getCoupon(code) {
+    try {
+      const coupon = await Coupon.findOne({ code });
+        if (!coupon) {
+            throw new ErrorResponse(
+            couponsErrors.COUPON_NOT_FOUND.message,
+            BAD_REQUEST,
+            couponsErrors.COUPON_NOT_FOUND.code
+            );
+        }
+
+        if(moment(coupon.expiryDate).isBefore(moment())) {
+            throw new ErrorResponse(
+            couponsErrors.INVALID_COUPON_EXPIRY_DATE.message,
+            BAD_REQUEST,
+            couponsErrors.INVALID_COUPON_EXPIRY_DATE.code,
+            );
+        }
+
+      return coupon;
+    } catch (e) {
+      logger.error(e);
+      throw e;
+    }
+  }
+
+  async createCoupon(body) {
+    try {
+        const isCouponExists = await Coupon.findOne({ code: body.code });
+
+        if (isCouponExists) {
+          if (moment(isCouponExists.expiryDate).isAfter(moment())) {
+            throw new ErrorResponse(
+              couponsErrors.COUPON_ALREADY_EXIST_AND_NOT_EXPIRED.message,
+              BAD_REQUEST,
+            couponsErrors.COUPON_ALREADY_EXIST_AND_NOT_EXPIRED.code
+            );
+          }
+        }     
+        
+        if (!moment(body.expiryDate).isValid() || moment(body.expiryDate).isBefore(moment())) {
+        throw new ErrorResponse(
+          couponsErrors.INVALID_COUPON_EXPIRY_DATE.message,
+          BAD_REQUEST,
+          couponsErrors.INVALID_COUPON_EXPIRY_DATE.code
+        );
+      }
+
+      const coupon = await Coupon.create(body);
+      return coupon;
+    } catch (e) {
+      logger.error(e);
+      throw e;
+    }
+  }
+
+  async updateCoupon(id, body) {
+    try {
+        const isCouponExist = await Coupon.findOne({ _id: id });
+        if (!isCouponExist) {
+            throw new ErrorResponse(
+            couponsErrors.COUPON_NOT_FOUND.message,
+            BAD_REQUEST,
+            couponsErrors.COUPON_NOT_FOUND.code
+            );
+        }
+
+      if (body.expiryDate) {
+        if (!moment(body.expiryDate).isValid() || moment(body.expiryDate).isBefore(moment())) {
+          throw new ErrorResponse(
+            couponsErrors.INVALID_COUPON_EXPIRY_DATE.message,
+            BAD_REQUEST,
+            couponsErrors.INVALID_COUPON_EXPIRY_DATE.code
+          );
+        }
+      }
+      const coupon = await Coupon.update({ _id: id }, body, { new: true });
+
+      return coupon;
+    } catch (e) {
+      logger.error(e);
+      throw e;
+    }
+  }
+
+  async deleteCoupon(id) {
+    try {
+      const isCouponExist = await Coupon.findOne({ _id: id });
+        if (!isCouponExist) {
+            throw new ErrorResponse(
+            couponsErrors.COUPON_NOT_FOUND.message,
+            BAD_REQUEST,
+            couponsErrors.COUPON_NOT_FOUND.code
+            );
+        }
+      const coupon = await Coupon.delete({ _id: id });
+      return coupon;
+    } catch (e) {
+      logger.error(e);
+      throw e;
+    }
+  }
+
+  async applyCoupon(code , cart) {
+    try {
+      const coupon = await Coupon.findOne({ code });
+        if (!coupon) {
+            throw new ErrorResponse(
+            couponsErrors.COUPON_NOT_FOUND.message,
+            BAD_REQUEST,
+            couponsErrors.COUPON_NOT_FOUND.code
+            );
+        }
+        if (moment(coupon.expiryDate).isBefore(moment())) {
+            throw new ErrorResponse(
+            couponsErrors.COUPON_NOT_FOUND.message,
+            BAD_REQUEST,
+            couponsErrors.COUPON_NOT_FOUND.code
+            );
+        }
+        if (coupon.status !== COUPON_STATUS.ACTIVE) {
+            throw new ErrorResponse(
+            couponsErrors.INVALID_COUPON_STATUS.message,
+            BAD_REQUEST,
+            couponsErrors.INVALID_COUPON_STATUS.code
+            );
+        }
+        if (coupon.code !== code) {
+            throw new ErrorResponse(
+            couponsErrors.INVALID_COUPON_CODE.message,
+            BAD_REQUEST,
+            couponsErrors.INVALID_COUPON_CODE.code
+            );
+        }
+
+        let discount = 0;
+        if (coupon.discount.type === COUPON_TYPES.PERCENTAGE) {
+            discount = (coupon.discount.value / 100) * cart.totalPrice;
+        } else {
+            discount = coupon.discount.value;
+        }
+
+        return discount;
+
+        
+    }
+    catch (e) {
+      logger.error(e);
+      throw e;
+    }
+
+}
+
+}
+
+export default new CouponService();
