7da7d024205f4af2782e4542023a773b7085da52
Izzedeen Alfarra <z4dev@Izzedeens-MacBook-Pro.local>
2024-10-21 16:27:30 +0300

    refactor:solve the issue

--- server/users/controller/index.js
+++ server/users/controller/index.js
@@ -64,7 +64,7 @@ export default {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        sameSite : 'lax' 
+        sameSite : 'Lax'  
       });
  
 
