c6a4b772740734759182019907f619ad49f7d982
Izzedeen Alfarra <z4dev@Izzedeens-MacBook-Pro.local>
2024-10-21 16:25:05 +0300

    refactor:solve the issue

--- server/users/controller/index.js
+++ server/users/controller/index.js
@@ -63,7 +63,8 @@ export default {
       res.cookie('refreshToken', newRefreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        maxAge: 7 * 24 * 60 * 60 * 1000
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        sameSite : 'lax' 
       });
  
 
