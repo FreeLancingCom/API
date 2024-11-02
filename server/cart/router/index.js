48a0e79e2f4be00a13c777d7cc28bf3c25be2f4e
Izzedeen Alfarra <z4dev@Izzedeens-MacBook-Pro.local>
2024-11-02 12:58:08 +0200

    added

--- server/cart/router/index.js
+++ server/cart/router/index.js
@@ -11,24 +11,18 @@ const router = express.Router();
 
 router.get(
   '/',
-  Authenticate,
-  Authorization.Authorize(Permissions[CONTROLLERS.LIST_CART]),
   validateRequest(validationSchemas[CONTROLLERS.LIST_CART]),
   Controller[CONTROLLERS.LIST_CART]
 );
 
 router.post(
   '/add-product/:id',
-  Authenticate,
-  Authorization.Authorize(Permissions[CONTROLLERS.CREATE_CART_PRODUCT]),
   validateRequest(validationSchemas[CONTROLLERS.CREATE_CART_PRODUCT]),
   Controller[CONTROLLERS.CREATE_CART_PRODUCT]
 );
 
 router.post(
   '/add-package/:id',
-  Authenticate,
-  Authorization.Authorize(Permissions[CONTROLLERS.CREATE_CART_PACKAGE]),
   validateRequest(validationSchemas[CONTROLLERS.CREATE_CART_PACKAGE]),
   Controller[CONTROLLERS.CREATE_CART_PACKAGE]
 );
@@ -37,8 +31,6 @@ router.post(
 // remove product from the cart
 router.delete(
   '/remove-product/:id',
-  Authenticate,
-  Authorization.Authorize(Permissions[CONTROLLERS.REMOVE_PRODUCT_FROM_CART]),
   validateRequest(validationSchemas[CONTROLLERS.REMOVE_PRODUCT_FROM_CART]),
   Controller[CONTROLLERS.REMOVE_PRODUCT_FROM_CART]
 );
@@ -46,8 +38,6 @@ router.delete(
 
 router.delete(
   '/remove-package/:id',
-  Authenticate,
-  Authorization.Authorize(Permissions[CONTROLLERS.REMOVE_PACKAGE_FROM_CART]),
   validateRequest(validationSchemas[CONTROLLERS.REMOVE_PACKAGE_FROM_CART]),
   Controller[CONTROLLERS.REMOVE_PACKAGE_FROM_CART]
 );
@@ -55,8 +45,6 @@ router.delete(
 // update product in the cart
 router.put(
   '/update-product/:id',
-  Authenticate,
-  Authorization.Authorize(Permissions[CONTROLLERS.UPDATE_CART_PRODUCT]),
   validateRequest(validationSchemas[CONTROLLERS.UPDATE_CART_PRODUCT]),
   Controller[CONTROLLERS.UPDATE_CART_PRODUCT]
 );
@@ -64,8 +52,6 @@ router.put(
 
 router.put(
   '/update-package/:id',
-  Authenticate,
-  Authorization.Authorize(Permissions[CONTROLLERS.UPDATE_CART_PACKAGE]),
   validateRequest(validationSchemas[CONTROLLERS.UPDATE_CART_PACKAGE]),
   Controller[CONTROLLERS.UPDATE_CART_PACKAGE]
 );
@@ -73,8 +59,6 @@ router.put(
 // apply coupon
 router.post(
   '/apply-coupon',
-  Authenticate,
-  Authorization.Authorize(Permissions[CONTROLLERS.APPLY_COUPON]),
   validateRequest(validationSchemas[CONTROLLERS.APPLY_COUPON]),
   Controller[CONTROLLERS.APPLY_COUPON]
 );
